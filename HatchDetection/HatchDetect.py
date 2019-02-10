0789b8f2d1c7749858dc2d305fb0176f76dc8c01
Medha P <medhap02@gmail.com>
2019-02-09 23:20:31 -0800

    almost done, need to do 4 box - Medha

--- HatchDetection/HatchDetect.py
+++ HatchDetection/HatchDetect.py
@@ -15,6 +15,7 @@ UPPER_GREEN_HUE = 80 # gimp: 300
 UPPER_GREEN_SAT = 255 # gimp: 100
 UPPER_GREEN_VAL = 255 # gimp: 100
 MIN_AREA = 4000
+
 #CAMERA_ANGLE = 90 #degrees, INCORRECT VALUE
 FOCAL_LENGTH = 1170.0 #2017 value
 CLOSEST_DIST = 8 # inches
@@ -48,6 +49,7 @@ class Boxes():
 
 rotated_boxes = []
 box_centers = []
+pairs = []
 
 #cap = cv2.VideoCapture(0)
 #change id above
@@ -60,7 +62,7 @@ while True:
     print("hello, no image, trying again")
     continue
    ''' 
-img = cv2.imread('hatch_5ft_3.jpg')
+img = cv2.imread('2019-02-09-152607.jpg')
 
 orig = img.copy()
 hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -68,6 +70,7 @@ green_range = cv2.inRange(hsv_img, lower_green, upper_green)
 green_range = cv2.GaussianBlur(green_range, (9,9),2)
 
 contours, hierarchy = cv2.findContours(green_range, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#img for serena?
 #print(contours)
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 #cv2.drawContours(orig,contours, -1, color, 3)
@@ -99,11 +102,11 @@ for i in range(len(contours)):
   rect_angle = rect[2]
   type = 0
   print (rect)
-  if (ANGLE_MIN_LEFT < rect_angle < ANGLE_MAX_LEFT) :
+  if (rect_angle <= -45):
     #left rotated boxes
     type = 1
     aspectRatio = rect_dim[1]/rect_dim[0]
-  elif (ANGLE_MIN_RIGHT < rect_angle < ANGLE_MAX_RIGHT):
+  elif (-45 < rect_angle):
      #right rotated boxes
      type = 2
      aspectRatio = rect_dim[0]/rect_dim[1]
@@ -123,9 +126,32 @@ for i in range(len(contours)):
     cv2.drawContours(orig, [contours[i]], 0, color, 2)
     cv2.drawContours(orig, [box],0,color,2)
   else: 
-    print ("huh")
+    print ("huh, where the box at?")
     continue
     
+
+
+
+print(len(rotated_boxes))
+for i in range(len(rotated_boxes)):
+  print (i)
+  box1 = rotated_boxes[i]
+  boxCoord = box1.boxes 
+  print("box")
+  print(box1.angle)
+  print(boxCoord)
+  
+
+print(len(rotated_boxes))
+
+for i in range(len(rotated_boxes)):
+ print(rotated_boxes[i].angle)
+ print("box")
+
+
+left_cnt = None
+right_cnt = None
+
 '''
 #check number of rotated boxes
 if len(rotated_boxes) < 2:
@@ -136,66 +162,55 @@ else:
   #more than 2, find correct pair
 '''
 
-print ("whatup dude")
 
-
-print(len(rotated_boxes))
-for i in range(len(rotated_boxes)):
-  print (i)
-  box1 = rotated_boxes[i] #left box
-  #box2 = rotated_boxes[i] #rightbox
-  boxCoord = box1.boxes
-  # topDiff = 
-  print("box")
-  print(box1.angle)
-  print(boxCoord)
-  
-
-print(len(rotated_boxes))
-
-for i in range(len(rotated_boxes)):
- #if ANGLE_MIN_LEFT < angle < ANGLE_MIN_RIGHT:
-
- print (rotated_boxes[i].angle)
- #print rotated_boxes[i]
- print("box")
-
-
-left_cnt = None
-right_cnt = None
-
-#print bound_rect[0]
-#print bound_rect[1]
 if len(rotated_boxes) < 2:
-  print ("bro, there aren't enough rotated boxes, please try again")
+  print("bro, there aren't enough rotated boxes, please try again")
   cv2.imshow('orig', orig)
+  cv2.imshow('green_range', green_range)
+  # TODO: maybe make it back up and run through again hatchdetect again
   '''
   plt.figure(0)
   plt.imshow(orig, cmap = 'gray', interpolation = 'bicubic')
-  plt.xticks([]), plt.yticks([])'''
+  plt.xticks([]), plt.yticks([])
 
-  #plt.figure(1)
-  #plt.imshow(green_range, cmap='gray', interpolation='bicubic')
-  cv2.imshow('green_range', green_range)
-  #plt.xticks([]), plt.yticks([])
-  #plt.show()
-  #continue
-elif bound_rect[0][0] < bound_rect[1][0]:
-  print ("lr1")
-  left_cnt = 0
-  right_cnt = 1
-else:
-  print("lr2")
-  left_cnt = 1
-  right_cnt = 0
+  plt.figure(1)
+  plt.imshow(green_range, cmap='gray', interpolation='bicubic')
+  plt.xticks([]), plt.yticks([])
+  plt.show()
+  continue
+  '''
 
-print("what is going on")
 
 if len(rotated_boxes) == 2:
-  print ("this is a okay")
+  print("got 2")
+  print(rotated_boxes[0].boxes[0][0])
+  print(rotated_boxes[1].boxes[0][0])
+  if (rotated_boxes[1].boxes[0][0] < rotated_boxes[0].boxes[0][0]) and (rotated_boxes[0].angle > (rotated_boxes[1].angle)):
+    print("'tis a pair'")
+    # TODO: maybe make it back up and run through again hatchdetect again
+  else:
+    print("sorry, it ain't a pair")
+
+number_of_pairs = 0
+
+if len(rotated_boxes) > 2:
+  print("excess boxes brotha")
+  for i in range(len(rotated_boxes)-1):
+    if (rotated_boxes[i+1].boxes[0][0] < rotated_boxes[i].boxes[0][0]) and (rotated_boxes[i].angle > (rotated_boxes[i+1].angle)):
+      print("yes")
+      #change above so works for four and correct angle orders
+      #pairs[0][0]=[i][i+1]
+
+'''
+box1 = rotated_boxes[i]
+  boxCoord = box1.boxes 
+  print("box")
+  print(box1.angle)
+  print(boxCoord)
+  '''
   
 
-  
+'''
 if len(bound_rect) >= 2:
   left_rightedge = bound_rect[left_cnt][0] + bound_rect[left_cnt][2]
   right_leftedge = bound_rect[right_cnt][0]
@@ -210,7 +225,7 @@ if len(bound_rect) >= 2:
 
   distance = CLOSEST_DIST * FOCAL_LENGTH / pixel_closest_dist
   #print(distance)
-
+'''
 #cv2.imshow('orig', orig)
 '''
 plt.figure(0)
